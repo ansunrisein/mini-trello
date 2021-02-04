d81cc12e813fcee4e289f50c19c36b4a41f55a8d
ansunrisein <ansunrisein@gmail.com>
2021-02-04 11:37:30 +0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
 import React from 'react'
+import {Redirect, Route, Switch} from 'react-router'
+import {BrowserRouter} from 'react-router-dom'
+import {BoardPage, Dashboard} from '@mt/pages'
+import {Template} from '@mt/ui'
 
-export const App: React.FC = () => <></>
+export const App: React.FC = () => (
+  <BrowserRouter>
+    <Template>
+      <Switch>
+        <Route path="/board/:id">
+          <BoardPage />
+        </Route>
+        <Route path="/boards">
+          <Dashboard />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/boards" />
+        </Route>
+      </Switch>
+    </Template>
+  </BrowserRouter>
+)
