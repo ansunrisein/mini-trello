99efc6beb6b2498308779edc77fae5ae2dda7d1b
ansunrisein <ansunrisein@gmail.com>
2021-02-04 11:33:24 +0300

    Implement dashboard page

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {connect, useSelector} from 'react-redux'
+import {Flex} from 'reflexbox'
+import {Link} from 'react-router-dom'
+import {TBoard, TBoardState} from '@mt/types'
+import {addBoard, removeBoard} from '@mt/store'
+import {Button} from '@mt/ui'
+import styles from './Dashboard.module.scss'
+
+export type DashboardProps = {
+  addNewBoard: () => void
+  removeBoard: (board: TBoard) => () => void
+}
+
+export const DashboardComponent: React.FC<DashboardProps> = ({addNewBoard, removeBoard}) => {
+  const boards = useSelector((state: TBoardState) => state.boards)
+
+  return (
+    <Flex flexDirection="column">
+      <Button className={styles.add} primary onClick={addNewBoard}>
+        Add board
+      </Button>
+      <div className={styles.grid}>
+        {boards.map(e => (
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            flexShrink={0}
+            className={styles.board}
+            key={e.id}
+          >
+            <Link to={`/board/${e.id}`}>
+              <h1 className={styles.title}>{e.title}</h1>
+            </Link>
+            <Button className={styles.remove} onClick={removeBoard(e)}>
+              Remove
+            </Button>
+          </Flex>
+        ))}
+      </div>
+    </Flex>
+  )
+}
+
+export const Dashboard = connect(null, dispatch => ({
+  addNewBoard: () => dispatch(addBoard()),
+  removeBoard: (board: TBoard) => () => dispatch(removeBoard(board)),
+}))(DashboardComponent)
--- src/pages/Dashboard/index.ts
+++ src/pages/Dashboard/index.ts
@@ -1,2 +1,1 @@
-export * from './BoardPage'
 export * from './Dashboard'
